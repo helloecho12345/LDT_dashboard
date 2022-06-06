4bb56276e60c822d625f3cf2ae41aa9d8e26beb3
Esther Cho <92740628+helloecho12345@users.noreply.github.com>
2022-06-06 11:44:04 +0100

    Project initialized in github

---
+++ main.js
@@ -0,0 +1,27 @@
+async function start() {
+  const form = document.getElementById("myForm");
+  // await fetch will wait for promise in fetch to resolve before continuing
+  // const response = await fetch("https://ldt-tech-test.herokuapp.com/api/startlistentries", {dataType: 'jsonp'})
+  const response = await fetch("/LDT_dashboard/startlistentries.json")
+  const data = await response.json();
+
+  createEventList(data)
+  console.log(data)
+  const html = data.map(race => {
+    return `
+    <div class="race">
+    <p>Race: ${race.eventTitle}</p>
+    <p>Name: ${race.firstName + " " + race.lastName}</p>
+    <p>Ticket Price Value: ${race.ticketPrice.value}</p>
+    </div>
+    `
+  }).join('')
+  document.querySelector("#app")
+  .insertAdjacentHTML("afterbegin", html);
+}
+
+start()
+
+function createEventList(eventList) {
+
+}
